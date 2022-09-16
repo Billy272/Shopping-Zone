871e8f66dea7d87fc60dbf92d40d2bc9c69749ad
Bill Bitok <98650586+Billy272@users.noreply.github.com>
2022-09-16 23:01:04 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,3 +4,17 @@ const scrollHeader = () =>{
                        : header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+let swiperProducts = new Swipper(".products-container",{
+    spaceBetween: 32,
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: true,
+    loop: true,
+
+
+    navigation: {
+        nextEl: ".swipper-button-next",
+        prevEl: ".swipper-button-prev",
+    },
+});
